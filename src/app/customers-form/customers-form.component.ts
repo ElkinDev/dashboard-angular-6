c2f6c6f7ec7d2cb452ad7cb0ae3dc8fbe412f70e
ElkinDev <elkinmendoza00@gmail.com>
2018-08-06 18:18:12 -0500

    Avances en el CRUD de formulario de clientes #22 #21 #20

--- src/app/customers-form/customers-form.component.ts
+++ src/app/customers-form/customers-form.component.ts
@@ -22,55 +22,35 @@ export class CustomersFormComponent implements OnInit {
   nameUserPhoto;
   fileToUpload: File = null;
   urlMainServer;
-  submittedFormPeople: boolean;
   typeId;
   ExistUser: boolean;
   RoleUser: string;
   urlMainServerPhotos;
-  newCustomerPeople = new FormGroup({
-    imgProfile: new FormControl(null),
-    nombre: new FormControl(null),
-    apellido: new FormControl(null),
-    mail: new FormControl(null),
-    address: new FormControl(null),
-    phone: new FormControl(null),
-    typeIdentification: new FormControl(null),
-    cedula: new FormControl(null),
-    checkmodusNew: new FormControl(null),
-  });
-  newCompanyCustomer = new FormGroup({
-    imgProfile: new FormControl(null),
-    nombre: new FormControl(null),
-    contactPerson: new FormControl(null),
-    mail: new FormControl(null),
-    address: new FormControl(null),
-    phone: new FormControl(null),
-    typeIdentification: new FormControl('NIT'),
-    cedula: new FormControl(null),
-    checkmodusNew: new FormControl(null),
-  });
+  fileImageEdit;
+  PowerPassword;
+  passwordinput;
+  newCustomerPeople: NgForm;
+  newCompanyCustomer: NgForm;
   mask: any[] = ['+57', '', '', ' ', '(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 
   constructor(private _location: Location, private cdRef: ChangeDetectorRef, private _CustomersService: CustomersService, private router: Router, private _FunctionsService: FunctionsService) {
     this.urlMainServerPhotos = environment.ws_url + '/public/dashboard/assets/images/'
-    this.urlMainServer = environment.ws_url + '/public/imgs/';
+    this.urlMainServer = environment.ws_url + '/public/imgs/'
     this.modusNewCustomer = 'Persona'
     this.statusNewCustomer = 'Activo'
     this.hrefImageUploaded = this.urlMainServer + 'noimage.png';
     this.NotEqualsPassword = false;
-    this.checkedActivoUser = false;
-    this.submittedFormPeople = false;
+    this.checkedActivoUser = true;
     this._FunctionsService.getAllDocumentsType().then(res => {
       this.typeId = res;
     }); this._FunctionsService.getAllDocumentsType().then(res => {
       this.typeId = res;
     });
-    this.newCustomerPeople.controls['typeIdentification'].setValue("C.C");
-    this.newCompanyCustomer.controls['typeIdentification'].setValue("C.C");
-    this.newCompanyCustomer.controls['checkmodusNew'].setValue(true);
-    this.newCustomerPeople.controls['checkmodusNew'].setValue(true);
     this.ExistUser = false;
     this.RoleUser = 'NaturalCustomer'
+    this.fileImageEdit = null;
+    this.PowerPassword = null;
+    this.passwordinput = '';
 
   }
   ngAfterViewChecked() {
@@ -81,34 +61,32 @@ export class CustomersFormComponent implements OnInit {
   backClicked() {
     this._location.back();
   }
-  changeModusUser() {
+  changeModusUser(newCustomerPeople: NgForm, newCompanyCustomer: NgForm) {
+
+    this.PowerPassword = null;
+    this.NotEqualsPassword = false;
+    this.statusNewCustomer = 'Activo'
+    this.hrefImageUploaded = this.urlMainServer + 'noimage.png';
     this.modusNewCustomer === 'Persona' ? this.modusNewCustomer = 'Empresa' : this.modusNewCustomer = 'Persona'
     if (this.modusNewCustomer != 'Persona') {
-      this.submittedFormPeople = false;
-      this.statusNewCustomer = 'Activo'
-      this.hrefImageUploaded = this.urlMainServer + 'noimage.png';
-      this.newCustomerPeople.controls['checkmodusNew'].setValue(true);
-      this.newCustomerPeople.controls['nombre'].setValue(null);
-      this.newCustomerPeople.controls['apellido'].setValue(null);
-      this.newCustomerPeople.controls['mail'].setValue(null);
-      this.newCustomerPeople.controls['address'].setValue(null);
-      this.newCustomerPeople.controls['phone'].setValue(null);
-      this.newCustomerPeople.controls['cedula'].setValue(null);
-      this.newCustomerPeople.controls['imgProfile'].setValue(null);
+      newCustomerPeople.setValue({
+        nombre: null,
+        imgProfile: null,
+        apellido: null,
+        address: null,
+        phone: null,
+        typeIdentification: 'CC',
+        cedula: null,
+        mail: null,
+        password: '',
+        passwordRepeat: '',
+        checkmodusNew: true
+
+      }); // or form.reset();
+
     } else {
-      this.newCompanyCustomer.controls['typeIdentification'].setValue("CC");
-      this.newCompanyCustomer.controls['checkmodusNew'].setValue(true);
-      this.newCompanyCustomer.controls['nombre'].setValue(null);
-      this.newCompanyCustomer.controls['contactPerson'].setValue(null);
-      this.newCompanyCustomer.controls['mail'].setValue(null);
-      this.newCompanyCustomer.controls['address'].setValue(null);
-      this.newCompanyCustomer.controls['phone'].setValue(null);
-      this.newCompanyCustomer.controls['cedula'].setValue(null);
-      this.newCompanyCustomer.controls['imgProfile'].setValue(null);
-      this.hrefImageUploaded = this.urlMainServer + 'noimage.png';
-      this.submittedFormPeople = false;
-      this.statusNewCustomer = 'Activo';
-      this.newCompanyCustomer.controls['checkmodusNew'].setValue(true);
+      console.log(newCompanyCustomer.value)
+
 
     }
   }
@@ -116,7 +94,6 @@ export class CustomersFormComponent implements OnInit {
     this.statusNewCustomer === 'Activo' ? this.statusNewCustomer = 'Inactivo' : this.statusNewCustomer = 'Activo'
   }
   onSubmitNewCustomerPeople(data: NgForm) {
-    this.submittedFormPeople = true;
     if (data.valid) {
       let senData = data.value;
       senData.RoleUser = this.RoleUser;
@@ -141,6 +118,7 @@ export class CustomersFormComponent implements OnInit {
   readUrl(event: any) {
     if (event.target.files && event.target.files[0]) {
       let file = event.target.files[0];
+      this.fileImageEdit = event.target.files[0];
       this.nameUserPhoto = file.name;
       var reader = new FileReader();
       reader.onload = (event: any) => {
@@ -149,5 +127,40 @@ export class CustomersFormComponent implements OnInit {
       reader.readAsDataURL(event.target.files[0]);
 
     }
+  }
+  ValidatePasswordPower(event: any) {
+    if (event.target.value) {
+      let statusPass = this._FunctionsService.ValidationSecurityPassword(event.target.value);
+      console.log('que es veamoslooo?', statusPass)
+      this.PowerPassword = statusPass;
+      switch (statusPass) {
+        case 'Débil':
+          break;
+        case 'Aceptable':
+          break;
+        case 'Fuerte':
+          break;
+      }
+      this.passwordinput = event.target.value;
+    } else {
+      console.log('se meteee');
+
+      this.PowerPassword = null;
+    }
+
+  }
+
+  validatePasswordRepeat(event: any, password) {
+    if (event.target.value != password.value) {
+      this.NotEqualsPassword = true;
+    } else {
+      this.NotEqualsPassword = false;
+    }
+    console.log('veamos el pass escrito', password);
+  }
+
+  disabledPassErr(): void {
+    this.NotEqualsPassword = false;
+
   }
 }
